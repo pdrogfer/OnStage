e14f5365698568bd9e13b34330a8b336aeee3ce7
Pedro Gonzalez Ferrandez <pdro74@hotmail.com>
2016-06-29 11:38:28 +0200

    refactoring SignInActivity-UserAuth in progress

--- app/src/main/java/com/pdrogfer/onstage/UserAuthFirebaseClient.java
+++ app/src/main/java/com/pdrogfer/onstage/UserAuthFirebaseClient.java
@@ -1,5 +1,22 @@
 package com.pdrogfer.onstage;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.pdrogfer.onstage.model.User;
+import com.pdrogfer.onstage.ui.GigsListActivity;
+import com.pdrogfer.onstage.ui.SignInActivity;
+
 /**
  * Created by pedrogonzalezferrandez on 29/06/16.
  *
@@ -9,8 +26,12 @@ public class UserAuthFirebaseClient {
 
     private static UserAuthFirebaseClient uniqueInstance;
 
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
 
     private UserAuthFirebaseClient() {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public static synchronized UserAuthFirebaseClient getInstance() {
@@ -18,6 +39,63 @@ public class UserAuthFirebaseClient {
             uniqueInstance = new UserAuthFirebaseClient();
         }
         return uniqueInstance;
+    }
+
+    public void checkAuth() {
+        if (mAuth.getCurrentUser() != null) {
+            onAuthSuccess(mAuth.getCurrentUser());
+        }
+    }
+
+    public void signIn(final Context context, String email, String password, final String artisticName) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(Utils.LOG_IN, "LogIn:onComplete:" + task.isSuccessful());
+                        
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(context, task.getResult().getUser(), artisticName);
+                        } else {
+                            Toast.makeText(context, "Log In Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void onAuthSuccess(Context context, FirebaseUser user, String artisticName) {
+        // String username = usernameFromEmail(user.getEmail());
+        String artisticUserName = artisticName;
+
+        // Write new user
+        writeNewUser(user.getUid(), artisticUserName, user.getEmail());
+
+
+        // TODO: 29/06/16
+        // Go to GigsListActivity
+        startActivity(new Intent(context, GigsListActivity.class));
+        finish();
+
+    }
+
+    // [START basic_write]
+    private void writeNewUser(String userId, String artisticName, String email) {
+        User user = new User(artisticName, email);
+        mDatabase.child("users").child(userId).setValue(user);
+    }
+    // [END basic_write]
+
+
+
+    // not used in my implementation, storing artisticName instead
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
     }
 
 }
--- app/src/main/java/com/pdrogfer/onstage/ui/SignInActivity.java
+++ app/src/main/java/com/pdrogfer/onstage/ui/SignInActivity.java
@@ -1,30 +1,18 @@
 package com.pdrogfer.onstage.ui;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.pdrogfer.onstage.R;
-import com.pdrogfer.onstage.model.User;
+import com.pdrogfer.onstage.UserAuthFirebaseClient;
 import com.pdrogfer.onstage.Utils;
 
 public class SignInActivity extends BaseActivity implements View.OnClickListener {
-
-    private DatabaseReference mDatabase;
-    private FirebaseAuth mAuth;
 
     private EditText mEmailField;
     private EditText mPasswordField;
@@ -32,13 +20,17 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
     private Button mLogInButton;
     private Button mRegisterButton;
 
+    private UserAuthFirebaseClient userAuth;
+
+    Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        mAuth = FirebaseAuth.getInstance();
+        userAuth = UserAuthFirebaseClient.getInstance();
+        context = this;
 
         // Views
         mEmailField = (EditText) findViewById(R.id.field_email);
@@ -57,9 +49,7 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
         super.onStart();
 
         // Check auth on Activity start
-        if (mAuth.getCurrentUser() != null) {
-            onAuthSuccess(mAuth.getCurrentUser());
-        }
+        userAuth.checkAuth();
     }
 
     private void logIn() {
@@ -72,21 +62,7 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
         String email = mEmailField.getText().toString();
         String password = mPasswordField.getText().toString();
 
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(Utils.LOG_IN, "LogIn:onComplete:" + task.isSuccessful());
-                        hideProgressDialog();
 
-                        if (task.isSuccessful()) {
-                            onAuthSuccess(task.getResult().getUser());
-                        } else {
-                            Toast.makeText(SignInActivity.this, "Log In Failed",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
     }
 
     private void register() {
@@ -98,43 +74,12 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
         showProgressDialog();
         String email = mEmailField.getText().toString();
         String password = mPasswordField.getText().toString();
+        String artisticName = mArtisticNameField.getText().toString();
 
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(Utils.LOG_IN, "createUser:onComplete:" + task.isSuccessful());
-                        hideProgressDialog();
-
-                        if (task.isSuccessful()) {
-                            onAuthSuccess(task.getResult().getUser());
-                        } else {
-                            Toast.makeText(SignInActivity.this, "Register Failed",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+        userAuth.signIn(context, email, password, artisticName);
     }
 
-    private void onAuthSuccess(FirebaseUser user) {
-        // String username = usernameFromEmail(user.getEmail());
-        String artisticUserName = mArtisticNameField.getText().toString();
 
-        // Write new user
-        writeNewUser(user.getUid(), artisticUserName, user.getEmail());
-
-        // Go to GigsListActivity
-        startActivity(new Intent(SignInActivity.this, GigsListActivity.class));
-        finish();
-    }
-
-    private String usernameFromEmail(String email) {
-        if (email.contains("@")) {
-            return email.split("@")[0];
-        } else {
-            return email;
-        }
-    }
 
     private boolean validateForm() {
         boolean result = true;
@@ -162,13 +107,7 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
         return result;
     }
 
-    // [START basic_write]
-    private void writeNewUser(String userId, String artisticName, String email) {
-        User user = new User(artisticName, email);
 
-        mDatabase.child("users").child(userId).setValue(user);
-    }
-    // [END basic_write]
 
     @Override
     public void onClick(View v) {
