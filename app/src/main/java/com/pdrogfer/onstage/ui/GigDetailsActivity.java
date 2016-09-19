21d628fe68fced3d8b2c0a39f9816e3e0ad454a4
pdrogfer <pedro.gf527@gmail.com>
2016-09-19 14:27:32 +0200

    added Map view to GigDetails

--- app/src/main/java/com/pdrogfer/onstage/ui/GigDetailsActivity.java
+++ app/src/main/java/com/pdrogfer/onstage/ui/GigDetailsActivity.java
@@ -7,12 +7,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.pdrogfer.onstage.R;
 
-public class GigDetailsActivity extends AppCompatActivity {
+public class GigDetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     public static final String EXTRA_GIG_DETAILS_KEY = "gig_details_key";
     private String gigIntentKey;
+
+    SupportMapFragment mapFragment;
+    GoogleMap googleMap;
+    boolean isMapReady;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,8 @@ public class GigDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gig_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initMapView();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         if (fab != null) {
@@ -46,5 +59,29 @@ public class GigDetailsActivity extends AppCompatActivity {
         // TODO: 09/09/2016 Initialize views from details layout, override OnStart and assign values to UI fields
 
 
+    }
+
+    private void initMapView() {
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentMap);
+        mapFragment.getMapAsync(this);
+        isMapReady = false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        isMapReady = true;
+        // TODO: 19/09/2016 get location from gig
+        double latitude = 38.3;
+        double longitude = -0.4;
+        String locationName = "this is the place";
+        moveMapToGigLocation(latitude, longitude, locationName);
+    }
+
+    private void moveMapToGigLocation(double latitude, double longitude, String locationName) {
+        // move map to location
+        LatLng location = new LatLng(latitude, longitude);
+        googleMap.addMarker(new MarkerOptions().position(location).title(locationName));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
     }
 }
