2ab00a6f6c975dcbab188125389bfaa80361a9d6
Pedro Gonzalez Ferrandez <pdro74@hotmail.com>
2016-10-12 10:41:45 +0200

    user check in Presentation, in progress, good so far

--- app/src/main/java/com/pdrogfer/onstage/ui/Presentation.java
+++ app/src/main/java/com/pdrogfer/onstage/ui/Presentation.java
@@ -1,13 +1,19 @@
 package com.pdrogfer.onstage.ui;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.stetho.Stetho;
 import com.pdrogfer.onstage.R;
+import com.pdrogfer.onstage.database.Contract;
+import com.pdrogfer.onstage.database.UsersContentProvider;
 
 public class Presentation extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,6 +30,8 @@ public class Presentation extends AppCompatActivity implements View.OnClickListe
         btnRegister = (Button) findViewById(R.id.button_register);
         btnLogin.setOnClickListener(this);
         btnRegister.setOnClickListener(this);
+
+        Stetho.initializeWithDefaults(this);
     }
 
     @Override
@@ -46,6 +54,26 @@ public class Presentation extends AppCompatActivity implements View.OnClickListe
     protected void onStart() {
         super.onStart();
 
-        // TODO: 04/10/2016 check auth to skip all the process for normal use
+        if (checkAuthActiveUser()) {
+            startActivity(new Intent(Presentation.this, GigsListActivity.class));
+        }
+    }
+
+    private boolean checkAuthActiveUser() {
+
+
+        Uri users = UsersContentProvider.CONTENT_URI;
+        // TODO: 12/10/16 use column user_active to find if the user was logged in already
+        // instead of tempName
+        String tempName = null;
+        Cursor cursor = getContentResolver().query(users, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                tempName = cursor.getString(cursor.getColumnIndex(Contract.COLUMN_NAME));
+                Toast.makeText(this, tempName, Toast.LENGTH_LONG).show();
+            } while (cursor.moveToNext());
+        }
+        
+        return (tempName != null);
     }
 }
